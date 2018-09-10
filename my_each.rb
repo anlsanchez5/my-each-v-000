4a1bbe10430c8b90fed6ca974eeed437f2191abf
anlsanchez@ucdavis.edu <anlsanchez@ucdavis.edu>
2018-09-10 21:56:19 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -2,9 +2,8 @@ def my_each(array)
  if block_given?
     i = 0
     while i < array.length
-      array.each do |index|
-        yield(array[i])
-        i = i + 1
+      yield(array[i])
+      i = i + 1
     end
 
     array
